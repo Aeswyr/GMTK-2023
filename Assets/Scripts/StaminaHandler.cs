849e5e4cfc530190f244454556cb6c5051ceecba
Camilo <crlimacastro@gmail.com>
2023-07-08 14:09:43 -0400

    Added basic stamina functionality

---
+++ Assets/Scripts/StaminaHandler.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class StaminaHandler : MonoBehaviour
+{
+    [SerializeField] private float moveCost = .001f;
+    [SerializeField] private float jumpCost = .10f;
+    [SerializeField] private float meowCost = .01f;
+
+    public void Increase(float value)
+    {
+        GameplayScreen.Instance.MeterCatStamina = Mathf.Clamp01(GameplayScreen.Instance.MeterCatStamina + value);
+    }
+
+    public void Decrease(float value)
+    {
+        GameplayScreen.Instance.MeterCatStamina = Mathf.Clamp01(GameplayScreen.Instance.MeterCatStamina - value);
+    }
+
+    public void DecreaseByMovementCost()
+    {
+        Decrease(moveCost);
+    }
+
+    public void DecreaseByJumpCost()
+    {
+        Decrease(jumpCost);
+    }
+
+    public void DecreaseByMeowCost()
+    {
+        Decrease(meowCost);
+    }
+}
