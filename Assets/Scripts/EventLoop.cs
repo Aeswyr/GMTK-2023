b46b3f6f76364ca3f714a937cc4e69d5fb13d9a2
Anthony <48633290+antuononewong@users.noreply.github.com>
2023-07-08 22:47:53 -0500

    Trigger speech bubble when Human gets hungry/sad

--- Assets/Scripts/EventLoop.cs
+++ Assets/Scripts/EventLoop.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class EventLoop : MonoBehaviour
 {
     [SerializeField] private GameObject human;
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject speechBubble;
+    [SerializeField] private GameObject example;
+    [SerializeField] private int duration;
     private HumanController humanController;
     private bool eventsStarted = false;
 
@@ -40,16 +44,24 @@ public class EventLoop : MonoBehaviour
     {
         var eventType = (EventType)Random.Range(0, System.Enum.GetNames(typeof(EventType)).Length);
         var nextEventTime = Random.Range(30, 46); // Every 60s in game is 1s
+        GameObject popUp = Instantiate(speechBubble, new Vector3(28f, -60f, 0f), Quaternion.identity);
+        popUp.transform.parent = canvas.transform;
+        popUp.transform.position = example.transform.position;
+        SpeechBubble popUpScript = popUp.GetComponent<SpeechBubble>();
 
         switch (eventType)
         {
             case EventType.LoseHappiness:
                 GameplayScreen.Instance.MeterHumanHappiness -= Random.Range(0.05f, 0.1f);
+                popUpScript.Initialize("I am sad.");
                 break;
             case EventType.GetHangry:
                 GameplayScreen.Instance.MeterHumanHunger -= Random.Range(0.1f, 0.2f);
+                popUpScript.Initialize("I hunger.");
                 break;
         }
+        Destroy(popUp, duration);
+
         yield return new WaitForSeconds(nextEventTime);
         StartCoroutine(EventRoutine());
     }
